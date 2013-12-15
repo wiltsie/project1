4eda9b36668d49f115db5208b213652a42f2612a
Liz Wiltsie <lizwiltsie@gmail.com>
2013-12-15 01:01:30 -0500

    add amazon S3 for paperclip

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'devise', '~> 3.1.1'
 
 gem 'paperclip', '~> 3.0'
 
+gem 'aws-sdk', '~> 1.20.0'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Pinteresting::Application.configure do
 
   #Set to actual Host
   config.action_mailer.default_url_options = { :host => 'wiltsie-pinteresting.com' }
+
+  #Set Paperclip to upload to Amazon S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
